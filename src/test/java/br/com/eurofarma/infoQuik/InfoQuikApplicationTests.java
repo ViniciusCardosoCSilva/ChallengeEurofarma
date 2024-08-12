809ea7efde38d9c7e8006b807107fc370ea3b542
gabguedes <gabriel4g.crispim@gmail.com>
2024-08-12 20:43:59 -0300

    criação da estrutura spring inicial

---
+++ src/test/java/br/com/eurofarma/infoQuik/InfoQuikApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eurofarma.infoQuik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoQuikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
